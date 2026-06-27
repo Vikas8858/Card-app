b05f7e00f459c88fd3501aa8f388c87be2f49dc6
Vikas Chauhan <vikasd8858@gmail.com>
2026-06-27 06:39:11 +0530

    Initial 2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,3 @@
-// import { useState } from "react";
-// import Form from "./components/Form";
-// import PanCard from "./components/PanCard";
-// import "./App.css";
-
-// function App() {
-
-//   const [data, setData] = useState({
-//     name: "",
-//     father: "",
-//     dob: "",
-//     pan: "",
-//     photo: ""
-//   });
-
-//   return (
-//     <div className="container">
-
-//       <h1>PAN Card Generator</h1>
-
-//       <Form data={data} setData={setData} />
-
-//       <PanCard data={data} />
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import { useState } from "react";
 import Form from "./components/Form";
 import PanCard from "./components/PanCard";
