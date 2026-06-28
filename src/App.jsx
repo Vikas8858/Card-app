a9c09035cea4334d9ceba7f44e308ddd20da6333
Vikas Chauhan <vikasd8858@gmail.com>
2026-06-28 20:03:45 +0530

    Fix Components folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Form from "./components/Form";
-import PanCard from "./components/PanCard";
+import Form from "./Components/Form";
+import PanCard from "./Components/PanCard";
 import "./App.css";
 
 function App() {
